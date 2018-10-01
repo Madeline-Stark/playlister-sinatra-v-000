656867483f1444d7cdb0ca8edca8774d08630df9
Madeline Stark <madeline.stark1@gmail.com>
2018-10-01 17:39:47 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -59,6 +59,7 @@ class SongsController < ApplicationController
     @song.artist = Artist.find_or_create_by(name: params["Artist Name"])
     @song.genre_ids = params["genres"]
     @song.save
+    flash[:message] = "Successfully updated song."
     erb :'songs/show'
   end
 
